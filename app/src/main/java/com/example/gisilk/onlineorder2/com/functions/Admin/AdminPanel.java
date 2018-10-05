75c0e20a63a44ab5c6321bb2e72568c16063ae60
Gihan Siriwardhana <gihan.siriwardhana@ifsworld.com>
2018-10-05 12:31:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gisilk/onlineorder2/com/functions/Admin/AdminPanel.java
@@ -0,0 +1,15 @@
+package com.example.gisilk.onlineorder2.com.functions.Admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gisilk.onlineorder2.R;
+
+public class AdminPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_panel);
+    }
+}
---
+++ app/src/main/java/com/example/gisilk/onlineorder2/com/functions/Booking/Booking.java
@@ -0,0 +1,15 @@
+package com.example.gisilk.onlineorder2.com.functions.Booking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gisilk.onlineorder2.R;
+
+public class Booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+    }
+}
